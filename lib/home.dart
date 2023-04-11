445ac0021df535869bbb385be53c3383a28afc19
RetroTho <74274119+RetroTho@users.noreply.github.com>
2023-04-11 12:46:58 -0700

    Implement basic temperature retrieval

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,36 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Weather> fetchWeather() async {
+  // La Verne, CA (91750) coords (lat,lon): 34.14776,-117.75206
+  double lat = 34.14776;
+  double lon = -117.75206;
+  String url = 'https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$lon&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=iso8601';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    return Weather.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to get weather');
+  }
+}
+
+class Weather {
+  final double temperature;
+
+  const Weather({
+    required this.temperature,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      temperature: json['current_weather']['temperature'],
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +40,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  late Future<Weather> futureWeather;
+
+  @override
+  void initState() {
+    super.initState();
+    futureWeather = fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Hello World!'),
+        child: FutureBuilder<Weather>(
+          future: futureWeather,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('${snapshot.data!.temperature}'),
+                ],
+              );
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
